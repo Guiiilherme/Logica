2ae5046c183dfd9c99f273546ad9d47b46879a6f
= <=>
2019-04-17 17:17:02 -0300

    jose assis

---
+++ Switch/site.js
@@ -0,0 +1,44 @@
+/**
+ * @author Arnaldo Victor Yamawaki Alves
+ */
+//var onde = window.location.pathname;
+//alert (onde)
+function abrirSite() {
+    let Entrada
+    Entrada = parseInt(frmAbrir.txtEntrada.value)
+    switch (Entrada) {
+        case 1:
+            window.location = "file:///C:/Users/guilherme.hacpaula/Pictures/Logica/calcImc/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 2:
+            window.location = "file:///C:/Users/guilherme.hacpaula/Pictures/Logica/fahrenheit%20para%20celsius/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 3:
+            window.location = "file:///C:/Users/guilherme.hacpaula/Pictures/Logica/churrasCalculadora/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 4:
+            window.location = "file:///C:/Users/guilherme.hacpaula/Pictures/Logica/Dolar/converterDolar.html";
+            break
+    }
+    switch (Entrada) {
+        case 5:
+            window.location = "file:///C:/Users/guilherme.hacpaula/Pictures/Logica/jogoDado/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 6:
+            window.location = "/C:/Users/arnaldo.vyalves/Documents/Logica-arnaldo/logica/precoDaHora/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 7:
+            window.location = "file:///C:/Users/guilherme.hacpaula/Pictures/Logica/areaCirculo/areaCirculo.html";
+            break
+    }
+}
